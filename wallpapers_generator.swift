c0aeac695901cbbd57e840f727c0dec84c01d9ad
Maxime Capelle <maxime.capelle@gmail.com>
2017-04-13 15:56:59 +0200

    Add iOS version from 10.2.0 to 10.3.1

--- wallpapers_generator.swift
+++ wallpapers_generator.swift
@@ -29,22 +29,22 @@ let iosVersions = [
     7, 7.01, 7.02, 7.03, 7.04, 7.05, 7.10, 7.11, 7.12,
     8.00, 8.01, 8.02, 8.10, 8.11, 8.12, 8.13, 8.20, 8.30, 8.40, 8.41,
     9.00, 9.01, 9.02, 9.10, 9.20, 9.21, 9.30, 9.31, 9.32, 9.33, 9.34, 9.35,
-    10.00, 10.01, 10.02, 10.03, 10.1, 10.11
+    10.00, 10.01, 10.02, 10.03, 10.1, 10.11, 10.20, 10.21, 10.30, 10.31
 ]
 
 let iDevices = [
     iDevice(name: "iPhone 4", resolution: (960, 640), firstIOSVersion: 7.00, lastIOSVersion: 8.41),
     iDevice(name: "iPhone 4S", resolution: (960,640), firstIOSVersion: 7.00, lastIOSVersion: 9.35),
-    iDevice(name: "iPhone 5", resolution: (1136, 640), firstIOSVersion: 7.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 5S", resolution: (1136, 640), firstIOSVersion: 7.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 6", resolution: (1334, 750), firstIOSVersion: 8.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 6 Plus", resolution: (1920, 1080), firstIOSVersion: 8.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 6S", resolution: (1334, 750), firstIOSVersion: 9.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 6S Plus", resolution: (1920, 1080), firstIOSVersion: 9.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 7", resolution: (1334, 750), firstIOSVersion: 10.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPhone 7 Plus", resolution: (1920, 1080), firstIOSVersion: 10.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPad Mini 4", resolution: (2048, 1536), firstIOSVersion: 9.00, lastIOSVersion: 10.11),
-    iDevice(name: "iPad Pro 9.7", resolution: (2048, 1536), firstIOSVersion: 9.3, lastIOSVersion: 10.11)
+    iDevice(name: "iPhone 5", resolution: (1136, 640), firstIOSVersion: 7.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 5S", resolution: (1136, 640), firstIOSVersion: 7.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 6", resolution: (1334, 750), firstIOSVersion: 8.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 6 Plus", resolution: (1920, 1080), firstIOSVersion: 8.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 6S", resolution: (1334, 750), firstIOSVersion: 9.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 6S Plus", resolution: (1920, 1080), firstIOSVersion: 9.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 7", resolution: (1334, 750), firstIOSVersion: 10.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPhone 7 Plus", resolution: (1920, 1080), firstIOSVersion: 10.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPad Mini 4", resolution: (2048, 1536), firstIOSVersion: 9.00, lastIOSVersion: 10.31),
+    iDevice(name: "iPad Pro 9.7", resolution: (2048, 1536), firstIOSVersion: 9.3, lastIOSVersion: 10.31)
 ]
 
 for iDevice in iDevices {
